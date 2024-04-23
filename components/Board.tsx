58c936b47431b5d349395d72a8e839ef79c86540
Tecu23 <andrei.teculescu@gmail.com>
2024-04-23 21:58:16 +0300

    update board to prepare for moving the pieces

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,51 +1,93 @@
+import { useRef } from "react";
+
 import PropTypes from "prop-types";
-import { pc2Text } from "../utils/config/globalConfig.js";
-import { Ranks, Files } from "../utils/constants/board.js";
-import Tile from "./Tile";
+
+import type { Square, PieceSymbol, Color, Piece } from "chess.js";
 import { Chess } from "chess.js";
 
-type Position = {
-    square: string;
-    type: string;
-    color: string;
-};
+import Tile from "./Tile";
 
-function Board({ name: string }) {
-    const board: Array<JSX.Element> = [];
+import { pc2Text } from "../utils/config/globalConfig.js";
+import { Ranks, Files } from "../utils/constants/board.js";
+
+function Board() {
+    const boardRef = useRef<HTMLDivElement>(null);
 
     const chess = new Chess();
     const b = chess.board();
 
-    console.log(b);
+    const grabPiece = (e: React.MouseEvent) => {
+        // const elem = e.target as HTMLElement;
+        // const board = boardRef.current;
+        // if (elem.classList.contains("chess-piece") && board) {
+        // }
+        console.log(e);
+    };
 
-    for (let r = 0; r < b.length; r++) {
-        for (let f = 0; f < b[0].length; f++) {
-            const sq: Position | null = b[r][f];
+    const movePiece = (e: React.MouseEvent) => {
+        console.log(e);
+    };
 
-            if (sq == null) {
-                console.log(Ranks[7 - r] + Files[f], Files[f].toLowerCase());
-                board.push(
-                    <Tile
-                        key={Ranks[7 - r] + Files[f].toLowerCase()}
-                        number={f + r}
-                    />,
-                );
-            } else {
-                board.push(
-                    <Tile
-                        image={`pieces/${pc2Text[sq.type]}_${sq.color}.png`}
-                        key={sq.square}
-                        number={f + r}
-                    />,
-                );
+    const dropPiece = (e: React.MouseEvent) => {
+        console.log(e);
+    };
+
+    const createBoard = (
+        b: Array<
+            Array<{
+                square: Square;
+                type: PieceSymbol;
+                color: Color;
+            } | null>
+        >,
+    ): Array<JSX.Element> => {
+        const board: Array<JSX.Element> = [];
+
+        for (let r = 0; r < b.length; r++) {
+            for (let f = 0; f < b[0].length; f++) {
+                const sq: {
+                    square: Square;
+                    type: PieceSymbol;
+                    color: Color;
+                } | null = b[r][f];
+
+                if (sq == null) {
+                    console.log(
+                        Ranks[7 - r] + Files[f],
+                        Files[f].toLowerCase(),
+                    );
+                    board.push(
+                        <Tile
+                            key={Ranks[7 - r] + Files[f].toLowerCase()}
+                            number={f + r}
+                        />,
+                    );
+                } else {
+                    board.push(
+                        <Tile
+                            image={`pieces/${pc2Text[sq.type]}_${sq.color}.png`}
+                            key={sq.square}
+                            number={f + r}
+                        />,
+                    );
+                }
             }
         }
-    }
+        return board;
+    };
+
+    const tiles = createBoard(b);
 
     return (
         <div className="bg-gray-500/60 w-full h-full flex justify-center items-center border rounded-lg">
-            <div className="w-[360px] h-[360px] bg-gray-600 grid grid-cols-8 grid-rows-8">
-                {board}
+            <div
+                onMouseMove={dropPiece}
+                onMouseDown={grabPiece}
+                onMouseUp={movePiece}
+                ref={boardRef}
+                className="w-[360px] h-[360px] bg-gray-600 grid grid-cols-8 grid-rows-8"
+            >
+                {tiles}
             </div>
         </div>
     );
