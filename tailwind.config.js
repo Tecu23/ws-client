0742fa82c37d3bc123e8e7b733ce0f7c6c9afdc3
Tecu23 <andrei.teculescu@gmail.com>
2024-11-01 09:13:24 +0200

    change formatting options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./utils/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        active: {
-          light: "#7B61FF80",
-          dark: "#7B61FF",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./utils/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                active: {
+                    light: "#7B61FF80",
+                    dark: "#7B61FF",
+                },
+                secondary: {
+                    DEFAULT: "#bac6fc",
+                },
+                square: {
+                    light: "#F4F7FA",
+                    dark: "#B7C0D8",
+                },
+                piece: {
+                    DEFAULT: "#34364C",
+                },
+            },
         },
-        secondary: {
-          DEFAULT: "#bac6fc",
-        },
-        square: {
-          light: "#F4F7FA",
-          dark: "#B7C0D8",
-        },
-        piece: {
-          DEFAULT: "#34364C",
-        },
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
